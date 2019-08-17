33d40b4115e1dd3a05b791d440c9ed79b2f41896
Jalen Adams <jalen@jalenkadams.me>
2019-08-17 10:02:31 -0400

    Add support for multireddits

--- reddit-wall/main.py
+++ reddit-wall/main.py
@@ -4,13 +4,49 @@ import praw
 import shutil
 
 
+def get_subreddits(config, reddit):
+    """Get the list of subreddits from the given config.
+    
+    Parameters
+    ----------
+    config : configparser.ConfigParser
+        The configuration to check.
+        
+    reddit : praw.Reddit
+        The reddit instance used to gather data.
+
+    Returns
+    -------
+    subreddits : set
+        Set of subreddit names.
+
+    """
+
+    subreddits = config.options("Subreddits")
+    for sub in subreddits:
+        sub = sub.lower()
+
+    multi_config = config.options("Multireddits")
+    multi_list = [tuple(m.split()) for m in multi_config]
+
+    for user, multi in multi_list:
+        multireddit = reddit.multireddit(user, multi)
+        for sub in multireddit.subreddits:
+            subreddits.append(sub.display_name.lower())
+
+    subreddits = set(subreddits)
+    return subreddits
+
+
 def main():
     """Main entry point of the program."""
+
     config = configparser.ConfigParser(allow_no_value=True)
     config.read(os.path.expanduser("~/.config/reddit-wall.conf"))
-
     reddit = praw.Reddit("reddit-wall")
-    subs = reddit.subreddit('+'.join(config.options("Subreddits")))
+
+    subreddits = get_subreddits(config, reddit)
+    subs = reddit.subreddit('+'.join(subreddits))
     limit = int(config["Downloads"]["ImageLimit"])
 
     for submission in subs.hot(limit=limit):
